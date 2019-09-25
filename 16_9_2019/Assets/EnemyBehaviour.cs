20c93d5604d826dcd490fbef394c37af1e4fadf2
mainguyenanhvu <1612904@student.hcmus.edu.vn>
2019-09-24 17:42:53 -0700

    Animation for character, background and enermy.

---
+++ 16_9_2019/Assets/EnemyBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    Vector2 pos;
+    float speed = 0.01f;
+    SpriteRenderer myspriteRenderer;
+    // Start is called before the first frame update
+    float limitLeft = 6.5f;
+    float limitRight = 11.5f;
+    void Start()
+    {
+        pos = transform.position;
+        myspriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.x += CheckStateDirect() * speed;
+        transform.position = pos;
+    }
+
+    private int CheckStateDirect()
+    {
+        if (pos.x > limitLeft && myspriteRenderer.flipX == false)
+            return -1;
+        if (pos.x < limitRight && myspriteRenderer.flipX == true)
+            return 1;
+        if (pos.x <= limitLeft && myspriteRenderer.flipX == false)
+        {
+            myspriteRenderer.flipX = true;
+            return 1;
+        }
+        if (pos.x >=limitRight && myspriteRenderer.flipX == true)
+        {
+            myspriteRenderer.flipX = false;
+            return -1;
+        }
+        return 0;
+    }
+}
---
+++ 16_9_2019/Assets/MarioNewBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioNewBehaviour : MonoBehaviour
+{
+    float speed = 0.1f;
+    Vector2 pos;
+    SpriteRenderer myspriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        myspriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (myspriteRenderer.flipX == true)
+            {
+                myspriteRenderer.flipX = false;
+            }
+            pos.x -= speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (myspriteRenderer.flipX == false)
+            {
+                myspriteRenderer.flipX = true;
+            }
+            pos.x += speed;
+        }
+        transform.position = pos;
+    }
+}
